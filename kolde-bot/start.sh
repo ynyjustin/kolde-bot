a5a461348551c6bffc18cf9b64468ac4bf5ce0e6
ynyjustin <cracksmarket69@gmail.com>
2025-03-24 22:56:56 +0200

    Delete kolde-bot directory

--- kolde-bot/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start the Discord bot in the background
-python bot.py &
-
-# Start the Flask webhook (or FastAPI) on port 10000
-gunicorn webhook:app --workers 1 --threads 8 -b 0.0.0.0:10000
